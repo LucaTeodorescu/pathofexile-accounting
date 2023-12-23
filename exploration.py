34049e9eb623c2434420732c345987ca4326868e
lucateodorescu <lvteodorescu@gmail.com>
2023-12-23 02:05:36 +0100

    test: made 2 df about the state of the stash

--- exploration.py
+++ exploration.py
@@ -1,1 +1,8 @@
-# This is the code calling the Path of Exile API to extract info over public and guild stashes
+# This is the code reading the GuildStashHistory and providing an analysis of the data
+
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+
+# Read the data
